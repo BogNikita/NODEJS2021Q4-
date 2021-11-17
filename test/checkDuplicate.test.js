6a2a6c4c62d0f65d123d0f0b5cc9e2ebdcdfccf7
Nikita Bogachev <nikita.bogachyov@noorsoft.ru>
2021-11-17 22:01:35 +0500

    add checkDuplicate test and custom error

---
+++ test/checkDuplicate.test.js
@@ -0,0 +1,17 @@
+const { expect } = require('@jest/globals');
+const checkDuplicate = require('../checkDuplicate');
+
+const ARG_WITH_DUPL = ['-c', '--config'];
+const ARG_WITHOUT_DUPL = ['-c', '--input'];
+
+describe('checkDuplicate', () => {
+
+  test('should dont have duplicate in arg', () => {
+    expect(checkDuplicate(ARG_WITHOUT_DUPL)).toBe(true);
+  });
+
+  test('should show error message', () => {
+    expect(() => checkDuplicate(ARG_WITH_DUPL)).toThrowError(new Error('Config includes Duplicate'));
+  });
+
+});
