974fe27f3bc8fc1769bd5f4388b504e1646a804a
Nikita Bogachev <nikita.bogachyov@noorsoft.ru>
2021-11-17 20:15:03 +0500

    fix cesar and rot8 ciphering

--- test/cipheringCesar.test.js
+++ test/cipheringCesar.test.js
@@ -1,15 +1,19 @@
 const { expect } = require('@jest/globals');
 const cipherinCesar = require('../cipheringCesar');
 
-const MOCK_DATA_UPPER_CASE = 'A';
-const MOCK_DATA_LOWER_CASE = 'a';
+const MOCK_DATA_UPPER_CASE_FIRST = 'A';
+const MOCK_DATA_UPPER_CASE_LAST = 'Z';
+const MOCK_DATA_LOWER_CASE_FIRST = 'a';
+const MOCK_DATA_LOWER_CASE_LAST = 'z';
 const MOCK_DATA_SYMBOLS = '1'
 const MOCK_DATA_RU = 'Й'
 
 describe('cipherinCesar', () => {
   test('should change char to next', () => {
-    expect(cipherinCesar.encode(MOCK_DATA_UPPER_CASE)).toBe('B');
-    expect(cipherinCesar.encode(MOCK_DATA_LOWER_CASE)).toBe('b');
+    expect(cipherinCesar.encode(MOCK_DATA_UPPER_CASE_FIRST)).toBe('B');
+    expect(cipherinCesar.encode(MOCK_DATA_LOWER_CASE_FIRST)).toBe('b');
+    expect(cipherinCesar.encode(MOCK_DATA_UPPER_CASE_LAST)).toBe('A');
+    expect(cipherinCesar.encode(MOCK_DATA_LOWER_CASE_LAST)).toBe('a');
   });
 
   test('should change char only latin alphabet', () => {
@@ -18,8 +22,10 @@ describe('cipherinCesar', () => {
   });
 
   test('should change char to prev', () => {
-    expect(cipherinCesar.decode(MOCK_DATA_UPPER_CASE)).toBe('Z');
-    expect(cipherinCesar.decode(MOCK_DATA_LOWER_CASE)).toBe('z');
+    expect(cipherinCesar.decode(MOCK_DATA_UPPER_CASE_FIRST)).toBe('Z');
+    expect(cipherinCesar.decode(MOCK_DATA_LOWER_CASE_FIRST)).toBe('z');
+    expect(cipherinCesar.decode(MOCK_DATA_UPPER_CASE_LAST)).toBe('Y');
+    expect(cipherinCesar.decode(MOCK_DATA_LOWER_CASE_LAST)).toBe('y');
   });
 
   test('should change input only latin alphabet', () => {
--- test/cipheringRot8.test.js
+++ test/cipheringRot8.test.js
@@ -1,15 +1,19 @@
 const { expect } = require('@jest/globals');
 const cipherinROT8 = require('../cipheringROT8');
 
-const MOCK_DATA_UPPER_CASE = 'A';
-const MOCK_DATA_LOWER_CASE = 'a';
+const MOCK_DATA_UPPER_CASE_FIRST = 'A';
+const MOCK_DATA_UPPER_CASE_LAST = 'Z';
+const MOCK_DATA_LOWER_CASE_FIRST = 'a';
+const MOCK_DATA_LOWER_CASE_LAST = 'z';
 const MOCK_DATA_SYMBOLS = '1'
 const MOCK_DATA_RU = 'Й'
 
 describe('cipherinROT8', () => {
   test('should change char to next', () => {
-    expect(cipherinROT8.encode(MOCK_DATA_UPPER_CASE)).toBe('I');
-    expect(cipherinROT8.encode(MOCK_DATA_LOWER_CASE)).toBe('i');
+    expect(cipherinROT8.encode(MOCK_DATA_UPPER_CASE_FIRST)).toBe('I');
+    expect(cipherinROT8.encode(MOCK_DATA_LOWER_CASE_FIRST)).toBe('i');
+    expect(cipherinROT8.encode(MOCK_DATA_UPPER_CASE_LAST)).toBe('H');
+    expect(cipherinROT8.encode(MOCK_DATA_LOWER_CASE_LAST)).toBe('h');
   });
 
   test('should change char only latin alphabet', () => {
@@ -18,8 +22,10 @@ describe('cipherinROT8', () => {
   });
 
   test('should change char to prev', () => {
-    expect(cipherinROT8.decode(MOCK_DATA_UPPER_CASE)).toBe('S');
-    expect(cipherinROT8.decode(MOCK_DATA_LOWER_CASE)).toBe('s');
+    expect(cipherinROT8.decode(MOCK_DATA_UPPER_CASE_FIRST)).toBe('S');
+    expect(cipherinROT8.decode(MOCK_DATA_LOWER_CASE_FIRST)).toBe('s');
+    expect(cipherinROT8.decode(MOCK_DATA_UPPER_CASE_LAST)).toBe('R');
+    expect(cipherinROT8.decode(MOCK_DATA_LOWER_CASE_LAST)).toBe('r');
   });
 
   test('should change input only latin alphabet', () => {
