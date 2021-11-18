a545a2bc3177c4428164914ac63c53a563d9701f
Nikita Bogachev <nikita.bogachyov@noorsoft.ru>
2021-11-18 20:55:40 +0500

    refactoring

--- parser.js
+++ parser.js
@@ -1,57 +1,42 @@
-const fs = require('fs');
 const checkDuplicate = require('./checkDuplicate');
+const ValidationError = require('./customError');
+const errorHandler = require('./errorHandler');
+const fileExist = require('./fileExist');
 
 function parser(arg) {
   const regEx = /^((A|(R(0|1))|(C(0|1)))-?)+$/;
+  try {
+    checkDuplicate(arg);
 
-  if (checkDuplicate(arg)) {
-    process.stderr.write('Config includes Duplicate');
-    process.exitCode = 4;
-    process.exit();
-  }
+    const configAliasIndex = arg.findIndex((el) => el === '-c' || el === '--config');
+    const inputAliasIndex = arg.findIndex((el) => el === '-i' || el === '--input');
+    const outputAliasIndex = arg.findIndex((el) => el === '-o' || el === '--output');
 
-  const configAliasIndex = arg.findIndex((el) => el === '-c' || el === '--config');
-  const inputAliasIndex = arg.findIndex((el) => el === '-i' || el === '--input');
-  const outputAliasIndex = arg.findIndex((el) => el === '-o' || el === '--output');
-
-  const config =
-    ['-i', '-o'].includes(arg[configAliasIndex + 1]) || configAliasIndex === -1
-      ? 'invalid'
-      : arg[configAliasIndex + 1];
-  const inputFile = ['-c', '-o'].includes(arg[inputAliasIndex + 1]) || inputAliasIndex === -1
-    ? 'stdin'
-    : arg[inputAliasIndex + 1];
-  const outputFile = ['-c', '-i'].includes(arg[outputAliasIndex + 1]) || outputAliasIndex === -1
-    ? 'stdout'
-    : arg[outputAliasIndex + 1];
+    const config =
+      ['-i', '-o'].includes(arg[configAliasIndex + 1]) || configAliasIndex === -1
+        ? 'invalid'
+        : arg[configAliasIndex + 1];
+    const inputFile =
+      ['-c', '-o'].includes(arg[inputAliasIndex + 1]) || inputAliasIndex === -1
+        ? 'stdin'
+        : arg[inputAliasIndex + 1];
+    const outputFile =
+      ['-c', '-i'].includes(arg[outputAliasIndex + 1]) || outputAliasIndex === -1
+        ? 'stdout'
+        : arg[outputAliasIndex + 1];
 
     if (config === 'invalid' || !config.match(regEx)) {
-    process.stderr.write('Invalid config');
-    process.exitCode = 2;
-    process.exit();
-  }
+      throw new ValidationError('Invalid config')
+    }
 
-  if (inputFile !== 'stdin') {
-    fs.access(inputFile, fs.constants.F_OK, (err) => {
-      if (err) {
-        process.stderr.write(`Input file ${inputFile} is not exist`);
-        process.exitCode = 3;
-        process.exit();
-      }
-    });
-  }
+    fileExist(inputFile, 'Input');
 
-  if (outputFile !== 'stdout') {
-    fs.access(outputFile, fs.constants.F_OK, (err) => {
-      if (err) {
-        process.stderr.write(`Output file ${outputFile} is not exist`);
-        process.exitCode = 3;
-        process.exit();
-      }
-    });
-  }
+    fileExist(outputFile, 'Output');
 
-  return { config, inputFile, outputFile };
+    return { config, inputFile, outputFile };
+  } catch (error) {
+    errorHandler(error);
+  }
 }
 
 module.exports = parser;
